f474954f435610fa46300528120f89cd1b192ec3
Mihika <98299545+MihikaG111@users.noreply.github.com>
2024-07-29 18:34:58 +0530

    nk

---
+++ @angular_material_select.js
@@ -0,0 +1,51 @@
+import {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  matSelectAnimations
+} from "./chunk-QXENBBPE.js";
+import "./chunk-4HZFXXUF.js";
+import {
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix
+} from "./chunk-HCCF7LMB.js";
+import "./chunk-CFF7J5QZ.js";
+import "./chunk-3HOJ5EAR.js";
+import {
+  MatOptgroup,
+  MatOption
+} from "./chunk-HQPVLHF3.js";
+import "./chunk-J365E3F2.js";
+import "./chunk-WGRT3FYF.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatOptgroup,
+  MatOption,
+  MatPrefix,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  MatSuffix,
+  matSelectAnimations
+};
+//# sourceMappingURL=@angular_material_select.js.map
